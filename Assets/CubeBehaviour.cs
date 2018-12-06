70378e87381f8fe30a54e365425dad64b01032aa
etn215n <43585438+etn215n@users.noreply.github.com>
2018-12-06 13:16:12 +0200

    First Commit

---
+++ Assets/CubeBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed = 70f;
+
+	void Update ()
+    {
+        transform.localEulerAngles += new Vector3(0f, rotationSpeed * Time.deltaTime, 0f);
+	}
+}
---
+++ Assets/InputEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class InputEvent : MonoBehaviour
+{
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Home))
+            SceneManager.LoadScene("MainMenu");
+        else if (Input.GetKeyDown(KeyCode.Escape))
+            SceneManager.LoadScene("MainMenu");
+    }
+}
